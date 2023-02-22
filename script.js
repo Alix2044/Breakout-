b1eda79fd423fa986f9093e8a4bf29d52c267f22
Alix2044 <Ali.jabarkhail@yahoo.com>
2023-02-22 10:44:40 +0100

    hh

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+
+let r = 20;
+let x = 400/2;
+let y = 400/2;
+
 function setup(){
     // When website starts
-    createCanvas(400, 400);
+    width = 400;
+    height = 400;
+    createCanvas(width, height);
   }
   
   function draw(){
     // Runs in a loop
     background(0);
+    circle(x, y, r*2)
+   
+
+    
+
   }
