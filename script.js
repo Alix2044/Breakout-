fed1f45722500809b8f755be51a9af8e569d28b0
Alix2044 <Ali.jabarkhail@yahoo.com>
2023-02-22 11:57:19 +0100

    script

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ let r = 20;
 let x = 400/2;
 let y = 400/2;
 
+var ball;
+var paddle;
+var brick;
+
+var score = 0;
+var play = false;
+
 function setup(){
     // When website starts
     width = 400;
     height = 400;
     createCanvas(width, height);
+    
+
   }
   
   function draw(){
