ddde25f23fda044f241ca1e0f7355bc5144853e5
Ihhy-ANVK <kristensena103@gmail.com>
2023-03-01 08:53:32 +0100

    sort code
    nemmere af overskue

--- paddle.js
+++ paddle.js
@@ -1,29 +1,29 @@
 
 
 function Paddle() {
-this.w = 20;
-this.h = 140;
-this.x = width/2;
-this.y = height/2 - this.h/2;
+    this.w = 20;
+    this.h = 140;
+    this.x = width/2;
+    this.y = height/2 - this.h/2;
 
-this.show = function(){
-fill(0,255,0)
-rect(this.x,this.y,this.w,this.h);
-}
-this.collision = function(b){
-    var ballLeft = b.x - b.r;
-    var ballRight = b.x + b.r;
-    var ballTop = b.y - b.r;
-    var ballBottom = b.y + b.r;
+    this.show = function(){
+        fill(0,255,0)
+        rect(this.x,this.y,this.w,this.h);
+    }
+    this.collision = function(b){
+        var ballLeft = b.x - b.r;
+        var ballRight = b.x + b.r;
+        var ballTop = b.y - b.r;
+        var ballBottom = b.y + b.r;
 
-    if(!(ballLeft>this.x+this.w||
-        ballRight<this.x ||
-        ballTop> this.y + this.height ||
-        ballBottom < this.y)){
-            b.vy = -b.vy
+        if(!(ballLeft > this.x + this.w
+            ||ballRight < this.x 
+            ||ballTop> this.y + this.height
+            ||ballBottom < this.y)){
+                b.vy = -b.vy
+
+        }
 
     }
-
-}
 
 }
